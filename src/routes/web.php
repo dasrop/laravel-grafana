4231b81b996db3477e43c000f4fd9eaa16f21875
minhajul <kmsohelinfo@gmail.com>
2020-08-13 22:21:58 +0600

    Updated README.md

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,19 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
-    for ($i = 0; $i < 1000; $i++){
-        \App\Datapoint::create([
-            'name' => 'temperature',
-            'value'=> 90.00,
-            'cast' => 'float'
-        ]);
-    }
-
     return view('welcome');
 });
-
-Route::post('/datapoint/create', [
-    'as'    => 'datapoints.store',
-    'uses'  => 'DatapointController@store'
-]);
