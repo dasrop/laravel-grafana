dfa24077670841aee56b924e5002fbe9302dbc2e
minhajul <kmsohelinfo@gmail.com>
2020-08-13 22:11:09 +0600

    Set laravel project and configured docker

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    for ($i = 0; $i < 1000; $i++){
+        \App\Datapoint::create([
+            'name' => 'temperature',
+            'value'=> 90.00,
+            'cast' => 'float'
+        ]);
+    }
+
     return view('welcome');
 });
 
