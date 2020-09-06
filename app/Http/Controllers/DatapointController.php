e1b510449a089d755d25627fffee1777584ec713
minhajul <kmsohelinfo@gmail.com>
2020-09-06 20:33:27 +0600

    Updated

--- app/Http/Controllers/DatapointController.php
+++ app/Http/Controllers/DatapointController.php
@@ -3,24 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Datapoint;
-use Illuminate\Http\Request;
 
 class DatapointController extends Controller
 {
     public function index()
     {
         return Datapoint::paginate(20);
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'value' => 'required'
-        ]);
-
-        return Datapoint::create(
-            $request->only('name', 'value', 'cast')
-        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DatapointController@index')->name('datapoint.index');
-Route::post('/', 'DatapointController@store')->name('datapoint.store');
